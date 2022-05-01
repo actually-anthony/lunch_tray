737e7d432d9d48461d6a7d6354e43bca1de8fc5b
Anthony <anthonyly@hotmail.ca>
2022-05-01 16:57:54 -0400

    start of project

---
+++ app/src/main/java/com/example/lunchtray/StartFragment.kt
@@ -0,0 +1,33 @@
+package com.example.lunchtray
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lunchtray.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+    private var binding: FragmentStartBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // used for data binding in fragment_start.xml
+        binding?.startFragment = this
+    }
+
+
+
+
+}
